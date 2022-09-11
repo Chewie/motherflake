2792baddb377f965bb654781c906becb277ff472
Kévin Sztern <sztern.kevin@gmail.com>
2022-09-11 10:12:48 +0200

    move bootloader stuff from workstation to host specific

--- nixos_configurations/common/workstation.nix
+++ nixos_configurations/common/workstation.nix
@@ -5,20 +5,6 @@
 { config, pkgs, ... }:
 
 {
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Bootloader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
-  boot.loader.grub.useOSProber = true;
-
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
